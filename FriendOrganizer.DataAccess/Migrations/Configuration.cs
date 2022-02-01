4cc341a6d32397c9663a802ebeb01bdfdd7f4d22
Jonathan Ball <jonathan.b@tnfs.co.za>
2022-02-01 14:50:17 +0200

    Final update

--- FriendOrganizer.DataAccess/Migrations/Configuration.cs
+++ FriendOrganizer.DataAccess/Migrations/Configuration.cs
@@ -1,53 +1,53 @@
 namespace FriendOrganizer.DataAccess.Migrations
 {
-  using FriendOrganizer.Model;
-  using System;
-  using System.Collections.Generic;
-  using System.Data.Entity;
-  using System.Data.Entity.Migrations;
-  using System.Linq;
+    using FriendOrganizer.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
 
-  internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
-  {
-    public Configuration()
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendOrganizer.DataAccess.FriendOrganizerDbContext>
     {
-      AutomaticMigrationsEnabled = false;
-    }
-
-    protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
-    {
-      context.Friends.AddOrUpdate(
-        f => f.FirstName,
-        new Friend { FirstName = "Thomas", LastName = "Huber" },
-        new Friend { FirstName = "Urs", LastName = "Meier" },
-        new Friend { FirstName = "Erkan", LastName = "Egin" },
-        new Friend { FirstName = "Sara", LastName = "Huber" }
-        );
-      context.ProgrammingLanguages.AddOrUpdate(
-        pl => pl.Name,
-        new ProgrammingLanguage { Name = "C#" },
-        new ProgrammingLanguage { Name = "TypeScript" },
-        new ProgrammingLanguage { Name = "F#" },
-        new ProgrammingLanguage { Name = "Swift" },
-        new ProgrammingLanguage { Name = "Java" });
-
-      context.SaveChanges();
-
-      context.FriendPhoneNumbers.AddOrUpdate(pn => pn.Number,
-        new FriendPhoneNumber { Number = "+49 12345678", FriendId = context.Friends.First().Id });
-
-      context.Meetings.AddOrUpdate(m => m.Title,
-        new Meeting
+        public Configuration()
         {
-          Title = "Watching Soccer",
-          DateFrom = new DateTime(2018, 5, 26),
-          DateTo = new DateTime(2018, 5, 26),
-          Friends = new List<Friend>
-          {
-            context.Friends.Single(f => f.FirstName == "Thomas" && f.LastName == "Huber"),
-            context.Friends.Single(f => f.FirstName == "Urs" && f.LastName == "Meier")
-          }
-        });
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+              f => f.FirstName,
+              new Friend { FirstName = "Jonathan", LastName = "Ball" },
+              new Friend { FirstName = "Pagan", LastName = "Ball" },
+              new Friend { FirstName = "Steve", LastName = "Quirk" },
+              new Friend { FirstName = "Erick", LastName = "Gerber" }
+              );
+            context.ProgrammingLanguages.AddOrUpdate(
+              pl => pl.Name,
+              new ProgrammingLanguage { Name = "C#" },
+              new ProgrammingLanguage { Name = "TypeScript" },
+              new ProgrammingLanguage { Name = "F#" },
+              new ProgrammingLanguage { Name = "Swift" },
+              new ProgrammingLanguage { Name = "Java" });
+
+            context.SaveChanges();
+
+            context.FriendPhoneNumbers.AddOrUpdate(pn => pn.Number,
+              new FriendPhoneNumber { Number = "+27 555 5556", FriendId = context.Friends.First().Id });
+
+            context.Meetings.AddOrUpdate(m => m.Title,
+              new Meeting
+              {
+                  Title = "Learn C#",
+                  DateFrom = new DateTime(2022, 4, 10),
+                  DateTo = new DateTime(2222, 4, 10),
+                  Friends = new List<Friend>
+                {
+            context.Friends.Single(f => f.FirstName == "Jonathan" && f.LastName == "Ball"),
+            context.Friends.Single(f => f.FirstName == "Steve" && f.LastName == "Quirk")
+                }
+              });
+        }
     }
-  }
 }
